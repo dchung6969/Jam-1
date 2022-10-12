da92ded51764da4e700ac968464d7267695a601a
dchung6969 <dchung6969@gmail.com>
2022-10-11 20:26:00 -0700

    added rock animations and player input

---
+++ docs/rockclimb/main.js
@@ -0,0 +1,127 @@
+title = "Rock Climb";
+
+description = `
+`;
+
+characters = [
+`
+l
+l l l
+ lll
+  l
+ l l
+ l  l
+`,`
+    l
+l l l
+ lll
+  l
+ l l
+l  l
+`,`
+ rrr
+rrRRr
+rrrRr
+rrrrr
+ rrr
+`
+];
+
+const G = {
+	WIDTH: 100,
+	HEIGHT: 150,
+	STAR_SPEED_MIN: 0.5,
+	STAR_SPEED_MAX: 1.0,
+	ROCK_MIN_BASE_SPEED: 1,
+	ROCK_MAX_BASE_SPEED: 2,
+	ROCK_ROTATION_SPEED: 0.1
+};
+
+options = {
+	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+};
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * mode: "left" | "right",
+ * }} Player
+ */
+
+/**
+ * @type { Player }
+ */
+let player;
+
+/**
+ * @typedef {{
+ * pos: Vector
+ * speed: number,
+ * rotation: number
+ * }} Rock
+ */
+
+/**
+ * @type { Rock [] }
+ */
+let rocks;
+
+/**
+ * @type { number }
+ */
+let currentRockSpeed;
+
+function update() {
+	if (!ticks) {
+		player = {
+			pos: vec(50, 130), 
+			mode: "left"
+		};
+
+		rocks = [];
+		currentRockSpeed = 0;
+	}
+	player.pos.clamp(0, G.WIDTH, 0, G.HEIGHT);
+	color("black");
+	const c1 = char(
+		player.mode === "left" || player.mode === "right" ? "a" : "b",
+		player.pos);
+	player.pos = vec(input.pos.x, 130);
+
+	if (rocks.length === 0) {
+		for (let i = 0; i < rnd(2,7); i ++) {
+			currentRockSpeed = rnd(G.ROCK_MIN_BASE_SPEED, G.ROCK_MAX_BASE_SPEED) * difficulty;
+			const posX = rnd(G.WIDTH);
+			const posY = rnd(0, 10);
+			rocks.push({
+				pos: vec(posX, posY),
+				speed: currentRockSpeed,
+				rotation: rnd(),
+			})
+		}
+	}
+	remove(rocks, (r) => {
+		r.pos.y += r.speed;
+		r.rotation += G.ROCK_ROTATION_SPEED;
+
+		color("red");
+		const isCollidingWithPlayer = char("c", r.pos, {rotation: r.rotation}).isColliding.char.c1;
+		particle(
+			r.pos.x,
+			r.pos.y,
+			0.5,
+			0.5,
+			PI/2,
+			PI/1.5
+		);
+		if (isCollidingWithPlayer) {
+			end();
+			play("powerUp");
+		}
+
+		if (r.pos.y > G.HEIGHT) {
+			addScore(r.speed);
+			return (r.pos.y > G.HEIGHT); 
+		}
+	});
+}
